b72db2aeefa3731eb503ad58b49ff87e826bf4c4
StefhaniAlkin <90950170+StefhaniAlkin@users.noreply.github.com>
2023-11-25 01:36:59 -0300

    test: should open edit modal after click on editar item button

---
+++ src/test/java/org/example/EditTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class EditTest {
+    WebDriver driver;
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        WebDriverManager.firefoxdriver().setup(); //sets up a valid driver for Chrome.
+    }
+
+    @Test
+    @DisplayName("Should open edit modal after click on editar item button")
+    void shouldOpenEditModalAfterClickOnEditarItemButton() throws InterruptedException {
+
+        driver.get("https://coffee-show.vercel.app/");
+        Thread.sleep(1000);
+        driver.findElement(By.id("update")).click();
+        WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
+        assertThat(modal).isNotNull();
+    }
+    
+}
