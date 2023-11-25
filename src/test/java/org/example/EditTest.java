13b8fc0e35db12b1544898a172162333600d3e10
StefhaniAlkin <90950170+StefhaniAlkin@users.noreply.github.com>
2023-11-25 20:32:39 -0300

    refactor(test): reorganize structure of EditTest with test suites

--- src/test/java/org/example/EditTest.java
+++ src/test/java/org/example/EditTest.java
@@ -1,10 +1,7 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -36,69 +33,66 @@ public class EditTest {
         }
     }
 
-    @Test
-    @DisplayName("Should open edit modal after click on editar item button")
-    void shouldOpenEditModalAfterClickOnEditarItemButton(){
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("update")));
-        button.click();
-        WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
-        assertThat(modal).isNotNull();
+    @Nested
+    @DisplayName("Edit Modal Tests")
+    class EditModalTests {
+
+        @Test
+        @DisplayName("Should open edit modal after click on editar item button")
+        void shouldOpenEditModalAfterClickOnEditarItemButton(){
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("update")));
+            button.click();
+            WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
+            assertThat(modal).isNotNull();
+        }
+        @Test
+        @DisplayName("Should close edit modal after click on close button")
+        void shouldCloseEditModalAfterClickOnFecharButton(){
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("update")));
+            button.click();
+            WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
+            WebElement primeiroBotao = modal.findElement(By.tagName("button"));
+            primeiroBotao.click();
+            assertThrows(NoSuchElementException.class, () -> {
+                driver.findElement(By.id("chakra-modal-:R1qpf6:"));
+            });
+        }
+        @Test
+        @DisplayName("Should open edit item modal after click on editar item button and select item to edit")
+        void shouldOpenEditItemModalAfterClickOnEditarItemButtonAndSelectItem() throws InterruptedException {
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("update")));
+            Thread.sleep(2000);
+            button.click();
+            WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
+            modal.findElement(By.xpath("//td[p[text()='Editar']]")).click();
+            WebElement itemModal = driver.findElement(By.id("chakra-modal-:r3:"));
+            assertThat(itemModal).isNotNull();
+        }
+        @Test
+        @DisplayName("Should show success toast after click on editar button inside item modal")
+        void shouldShowSuccessToastAfterClickOnEditarButtonInsideItemModal() throws InterruptedException {
+            Thread.sleep(2000);
+            driver.findElement(By.id("update")).click();
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td[5]//p")));
+            button.click();
+            WebElement itemModal = driver.findElement(By.id("chakra-modal-:r3:"));
+            itemModal.findElement(By.className("chakra-button")).click();
+            WebElement toast = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("toast-1-title")));
+            assertThat(toast.getText()).isEqualTo("Item alterado com sucesso!");
+        }
+        @Test
+        @DisplayName("Should alert if any input of form is empty after click on editar button")
+        void shouldAlertIfAnyInputOfFormIsEmptyAfterClickOnEditarButton() throws InterruptedException {
+            Thread.sleep(3000);
+            driver.findElement(By.id("update")).click();
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td[5]//p")));
+            button.click();
+            WebElement itemModal = driver.findElement(By.id("chakra-modal-:r3:"));
+            WebElement itemNameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='chakra-modal--body-:r3:']//input[1]")));
+            itemNameInput.clear();
+            itemModal.findElement(By.className("chakra-button")).click();
+            WebElement errorDiv = driver.findElement(By.className("chakra-form__error-message"));
+            assertThat(errorDiv).isNotNull();
+        }
     }
-
-    @Test
-    @DisplayName("Should close edit modal after click on close button")
-    void shouldCloseEditModalAfterClickOnFecharButton(){
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("update")));
-        button.click();
-        WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
-
-        WebElement primeiroBotao = modal.findElement(By.tagName("button"));
-        primeiroBotao.click();
-        assertThrows(NoSuchElementException.class, () -> {
-            driver.findElement(By.id("chakra-modal-:R1qpf6:"));
-        });
-    }
-
-    @Test
-    @DisplayName("Should open edit item modal after click on editar item button and select item to edit")
-    void shouldOpenEditItemModalAfterClickOnEditarItemButtonAndSelectItem() throws InterruptedException {
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("update")));
-        Thread.sleep(2000);
-        button.click();
-        WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
-        modal.findElement(By.xpath("//td[p[text()='Editar']]")).click();
-        WebElement itemModal = driver.findElement(By.id("chakra-modal-:r3:"));
-        assertThat(itemModal).isNotNull();
-    }
-
-    @Test
-    @DisplayName("Should show success toast after click on editar button inside item modal")
-    void shouldShowSuccessToastAfterClickOnEditarButtonInsideItemModal() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.findElement(By.id("update")).click();
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td[5]//p")));
-        button.click();
-        WebElement itemModal = driver.findElement(By.id("chakra-modal-:r3:"));
-        itemModal.findElement(By.className("chakra-button")).click();
-        WebElement toast = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("toast-1-title")));
-        assertThat(toast.getText()).isEqualTo("Item alterado com sucesso!");
-    }
-
-
-    @Test
-    @DisplayName("Should alert if any input of form is empty after click on editar button")
-    void shouldAlertIfAnyInputOfFormIsEmptyAfterClickOnEditarButton() throws InterruptedException {
-        Thread.sleep(3000);
-        driver.findElement(By.id("update")).click();
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td[5]//p")));
-        button.click();
-        WebElement itemModal = driver.findElement(By.id("chakra-modal-:r3:"));
-        WebElement itemNameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='chakra-modal--body-:r3:']//input[1]")));
-        itemNameInput.clear();
-        itemModal.findElement(By.className("chakra-button")).click();
-        WebElement errorDiv = driver.findElement(By.className("chakra-form__error-message"));
-        assertThat(errorDiv).isNotNull();
-    }
-
-
 }
