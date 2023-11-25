0038e4283a0b9196d084856f184c6571ba065414
StefhaniAlkin <90950170+StefhaniAlkin@users.noreply.github.com>
2023-11-25 20:35:05 -0300

    refactor(test): reorganize structure of RemoveTest with test suites

--- src/test/java/org/example/RemoveTest.java
+++ src/test/java/org/example/RemoveTest.java
@@ -1,10 +1,7 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -40,67 +37,67 @@ public class RemoveTest {
         }
     }
 
-    @Test
-    @DisplayName("Should open remove modal after click on remove item button")
-    void shouldOpenEditModalAfterClickOnEditarItemButton(){
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("delete")));
-        button.click();
-        WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
-        assertThat(modal).isNotNull();
-    }
+    @Nested
+    @DisplayName("Remove Modal Tests")
+    class RemoveModalTests{
+        @Test
+        @DisplayName("Should open remove modal after click on remove item button")
+        void shouldOpenEditModalAfterClickOnEditarItemButton(){
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("delete")));
+            button.click();
+            WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
+            assertThat(modal).isNotNull();
+        }
+        @Test
+        @DisplayName("Should close remove modal after click on close button")
+        void shouldCloseRemoveModalAfterClickOnFecharButton(){
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("delete")));
+            button.click();
+            WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
 
-    @Test
-    @DisplayName("Should close remove modal after click on close button")
-    void shouldCloseRemoveModalAfterClickOnFecharButton(){
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("delete")));
-        button.click();
-        WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
-
-        WebElement primeiroBotao = modal.findElement(By.tagName("button"));
-        primeiroBotao.click();
-        assertThrows(NoSuchElementException.class, () -> {
-            driver.findElement(By.id("chakra-modal-:R1qpf6:"));
-        });
-    }
-
-    @Test
-    @DisplayName("Should open remove item modal after click on delete button")
-    void shouldOpenRemoveItemModalAfterClickOnDeleteButton() throws InterruptedException {
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("delete")));
-        Thread.sleep(3000);
-        button.click();
-        WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
-        modal.findElement(By.xpath("//td[p[text()='Deletar']]")).click();
-        WebElement itemModal = driver.findElement(By.id("chakra-modal-:r3:"));
-        assertThat(itemModal).isNotNull();
-    }
-
-    @Test
-    @DisplayName("Should remove item with success after click on delete button")
-    void shouldRemoveItemWithSuccessAfterClickOnDeleteButton() throws InterruptedException {
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("delete")));
-        Thread.sleep(3000);
-        button.click();
-        WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
-        modal.findElement(By.xpath("//td[p[text()='Deletar']]")).click();
-        WebElement deletarButtonModal = wait.until(ExpectedConditions.elementToBeClickable(By.className("chakra-button")));
-        deletarButtonModal.click();
-        WebElement toast = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("toast-1-title")));
-        assertThat(toast.getText()).isEqualTo("Item deletado com sucesso!");
-    }
-
-    @Test
-    @DisplayName("Should close delete confirmation modal after click on close button")
-    void shouldCloseDeleteConfirmationModalAterClickOnCloseBUtton() throws InterruptedException {
-        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("delete")));
-        Thread.sleep(3000);
-        button.click();
-        WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
-        modal.findElement(By.xpath("//td[p[text()='Deletar']]")).click();
-        List<WebElement> closeButtons = driver.findElements(By.className("chakra-modal__close-btn"));
-        closeButtons.get(1).click();
-        List<WebElement> modals = driver.findElements(By.className("chakra-modal__content-container"));
-        assertThrows(IndexOutOfBoundsException.class, () -> {
-            modals.get(1);});
+            WebElement primeiroBotao = modal.findElement(By.tagName("button"));
+            primeiroBotao.click();
+            assertThrows(NoSuchElementException.class, () -> {
+                driver.findElement(By.id("chakra-modal-:R1qpf6:"));
+            });
+        }
+        @Test
+        @DisplayName("Should open remove item modal after click on delete button")
+        void shouldOpenRemoveItemModalAfterClickOnDeleteButton() throws InterruptedException {
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("delete")));
+            Thread.sleep(3000);
+            button.click();
+            WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
+            modal.findElement(By.xpath("//td[p[text()='Deletar']]")).click();
+            WebElement itemModal = driver.findElement(By.id("chakra-modal-:r3:"));
+            assertThat(itemModal).isNotNull();
+        }
+        @Test
+        @DisplayName("Should remove item with success after click on delete button")
+        void shouldRemoveItemWithSuccessAfterClickOnDeleteButton() throws InterruptedException {
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("delete")));
+            Thread.sleep(3000);
+            button.click();
+            WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
+            modal.findElement(By.xpath("//td[p[text()='Deletar']]")).click();
+            WebElement deletarButtonModal = wait.until(ExpectedConditions.elementToBeClickable(By.className("chakra-button")));
+            deletarButtonModal.click();
+            WebElement toast = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("toast-1-title")));
+            assertThat(toast.getText()).isEqualTo("Item deletado com sucesso!");
+        }
+        @Test
+        @DisplayName("Should close delete confirmation modal after click on close button")
+        void shouldCloseDeleteConfirmationModalAterClickOnCloseBUtton() throws InterruptedException {
+            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id("delete")));
+            Thread.sleep(3000);
+            button.click();
+            WebElement modal = driver.findElement(By.id("chakra-modal-:R1qpf6:"));
+            modal.findElement(By.xpath("//td[p[text()='Deletar']]")).click();
+            List<WebElement> closeButtons = driver.findElements(By.className("chakra-modal__close-btn"));
+            closeButtons.get(1).click();
+            List<WebElement> modals = driver.findElements(By.className("chakra-modal__content-container"));
+            assertThrows(IndexOutOfBoundsException.class, () -> {
+                modals.get(1);});
+        }
     }
 }
